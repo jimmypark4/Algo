c3bab869a2b785ec300924d8501a0bd6b03ef0e2
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-01-30 17:28:52 +0900

    [Silver II] Title: 이항 계수 2, Time: 432 ms, Memory: 87420 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -1,17 +1,18 @@
 #11051 이항계수
 #b(n,0)=b(n,n)=1
 #b(n,r)=b(n-1,r-1)+b(n-1,r)
-from itertools import combinations
-import sys
-sys.setrecursionlimit(10**7)
+#bottom up 방식
 N,K = map(int,input().split())
 cache=[[0]*(N+1) for _ in range(N+1)]
+#N*N의 이차원 배열을 생성함
 for i in range(N+1):
-    cache[i][0]=1
-    cache[i][i]=1
+    cache[i][0] = 1
+    cache[i][i] = 1
+    for j in range(N+1):
+        if cache[i][j] ==0:
+            cache[i][j]=cache[i-1][j-1]+cache[i-1][j]
+   # print(cache[i])
 def b(n,r):
-    if cache[n][r] == 0:
-        cache[n][r]=b(n - 1, r - 1) + b(n - 1, r)
     return cache[n][r]
 
 result = b(N,K)%10007
