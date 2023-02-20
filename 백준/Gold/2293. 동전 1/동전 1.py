f2adaee6af1a068e61b901e0334cfb8248d304b1
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-02-20 20:02:08 +0900

    [Gold V] Title: 동전 1, Time: 228 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -4,6 +4,8 @@ coins = [int(input()) for _ in range(n)]
 dp = [0 for _ in range(k+1)]
 dp[0] = 1
 # print(n, k, coins)
+#코인의 경우의수를 생각해서
+#코인보다 지금 k가 적으면 더하고 아니면 걍 넘기기
 for coin in coins:
     for i in range(coin,k+1):
         if i-coin>=0:
