91e3c192db2a722523c1f8f3e2925a04564d5b31
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-02-22 16:11:40 +0900

    [Silver I] Title: 오르막 수, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,22 @@
+#11057 오르막수
+# 10007로 나누는거 보니 수가 졸라크고
+# n에 의해 각기 규칙이 있어보이니
+# DP로 해봅시다.
+
+N = int(input())
+
+dp = [[0 for i in range(10)] for i in range(N)]
+# print(*dp,sep="\n")
+
+for i in range(1):
+    for j in range(10):
+            dp[i][j] = 1
+
+
+for i in range(1,N):
+    for j in range(0,10):
+        for k in range(j,10):
+            dp[i][j] += dp[i-1][k]
+# print(*dp,sep="\n")
+
+print(sum(dp[N-1])%10007)
