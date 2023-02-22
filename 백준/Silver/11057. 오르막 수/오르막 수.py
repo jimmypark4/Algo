7e9c5ab77ad142c55acea81e38a76ee782dba6eb
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-02-22 16:35:26 +0900

    [Silver I] Title: 오르막 수, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -6,17 +6,15 @@
 N = int(input())
 
 dp = [[0 for i in range(10)] for i in range(N)]
-# print(*dp,sep="\n")
-
 for i in range(1):
     for j in range(10):
             dp[i][j] = 1
 
-
 for i in range(1,N):
     for j in range(0,10):
-        for k in range(j,10):
-            dp[i][j] += dp[i-1][k]
+       dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        # for k in range(j,10):
+        #     dp[i][j] += dp[i-1][k]
 # print(*dp,sep="\n")
 
 print(sum(dp[N-1])%10007)
