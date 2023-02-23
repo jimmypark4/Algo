37bd7f44e1665d9d544048bebe62975a892856ec
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-02-23 17:28:13 +0900

    [Silver I] Title: 동물원, Time: 636 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -1,10 +1,5 @@
 N = int(input())
-dp = [0] * 2
-dp[0] = 1
-dp[1] = 2
-
+dp = [ i+1 for i in range(2)] 
 for _ in range(1,N):
   dp[0] , dp[1] = dp[0] + dp[1] , 2 * dp[0] + dp[1]
-
-
 print(sum(dp)%9901)
