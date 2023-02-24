7957399cbeadd73d352d6a6e7821c2ce76c48ff2
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-02-24 16:40:07 +0900

    [Silver III] Title: N과 M (3), Time: 1384 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -2,11 +2,14 @@
 n, m = map(int, input().split())
 s = []
 def dfs():
+    #길이가 되면 지금까지 모은거 프린트 하고 리턴!
     if len(s) == m:
         print(' '.join(map(str, s)))
         return
+    #1부터 n 까지 돌면서
+    #넣고 재귀호출 넣고 재귀호출 
     for i in range(1, n + 1):
         s.append(i)
-        dfs()
+        dfs() #m되서 끝나면 그전에꺼 팝하고 다시 2부터 시작!
         s.pop()
 dfs()
