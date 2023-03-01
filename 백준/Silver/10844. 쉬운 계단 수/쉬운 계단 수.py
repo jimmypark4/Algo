df52497805c5d9766a1502cd036e6ef87cb5d0d6
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-03-01 16:50:38 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,17 +1,23 @@
 #10844 쉬운 계단수
-# N = map(int, input().split())
-N= int(input())
-dp = [[ 0 for _ in range(10)] for _ in range(N)]
-for j in range(0,10):
-    dp[0][j] = 1
-for i in range(1,N):
-    for j in range(10):
-        if j==0:
-            dp[i][j] +=  dp[i-1][j+1]
-        elif j==9:
-            dp[i][j] += dp[i-1][j-1]
-        else:
-            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+# dp
 
-# print(*dp,sep="\n")
-print(sum(dp[N-1][1:]) % 1000000000)
+mod = 1000000000
+
+dp = [[0 for _ in range(10)] for _ in range(101)]
+
+for k in range(0,9+1):
+    dp[1][k] = 1
+
+for n in range(2,100+1):
+    for k in range(10):
+        if k == 0:
+            dp[n][k] = dp[n-1][k+1]
+        elif k== 9:
+            dp[n][k] = dp[n-1][k-1]
+        else:
+            dp[n][k] = dp[n-1][k-1] + dp[n-1][k+1]
+
+
+N = int(input())
+
+print(sum(dp[N][1:]) % mod)
