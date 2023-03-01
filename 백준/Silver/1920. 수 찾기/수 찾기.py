142c5c593c901e88cab2cf4cc766f839012a3c49
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-03-01 20:24:39 +0900

    [Silver IV] Title: 수 찾기, Time: 172 ms, Memory: 50404 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,13 @@
+#10815 숫자 카드
+
+n = int(input())  # 상근이가 가지고 있는 숫자 카드의 개수
+cards = set(map(int, input().split()))  # 상근이가 가지고 있는 숫자 카드
+m = int(input())  # 찾아야 할 숫자의 개수
+numbers = list(map(int, input().split()))  # 찾아야 할 숫자
+
+# set을 이용하여 찾고자 하는 숫자가 상근이가 가지고 있는 숫자 카드에 있는지 확인
+for number in numbers:
+    if number in cards:
+        print(1)
+    else:
+        print(0)
