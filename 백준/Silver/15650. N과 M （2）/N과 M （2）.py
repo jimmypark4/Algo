ec944bf51614e6b1505f37a589271fce8850bcff
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-05-04 17:33:26 +0900

    [Silver III] Title: N과 M (2), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,11 +1,17 @@
-#15650 N과 M(2)
-# 오름차순으로 출력하는 것이므로
-#BFS or DFS일 것같다.
-#오름차순 모든 경우의 수 출력 => BFS
+import sys
+input = sys.stdin.readline
 
-#편법
-from itertools import combinations
-N,M = map(int,input().split())
-arr = [i for i in range(1,N+1)]
-for combi in combinations(arr,M):
-    print(*combi,sep=" ")
+n, m = map(int, input().split())
+arr = []
+
+def dfs(start, depth):
+    if depth == m:
+        print(*arr)
+        return
+
+    for i in range(start, n+1):
+        arr.append(i)
+        dfs(i+1, depth+1)
+        arr.pop()
+
+dfs(1, 0)
