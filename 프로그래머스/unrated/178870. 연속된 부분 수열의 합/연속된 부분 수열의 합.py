e5a3301f9cd7ca27c2ee18de5e09ea10eb5f5878
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-06-21 18:46:53 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,22 +1,27 @@
 def solution(sequence, k):
-    answers = []
-    sum = 0
-    l = 0
-    r = -1
-    
+    answer = []
+    s,e =0,0
+    total = sequence[0]
     while True:
-        if sum < k:
-            r += 1
-            if r >= len(sequence):
+        if total == k:
+            answer.append([s,e])
+            
+        if s > e:
+            s,e =e,s
+            total = sum(sequence[s:e+1])
+        
+        
+        if total < k:
+            e += 1
+            if e >= len(sequence):
                 break
-            sum += sequence[r]
-        else:
-            sum -= sequence[l]
-            if l >= len(sequence):
+            total += sequence[e]
+        
+        elif total >= k:            
+            total -= sequence[s]
+            s += 1
+            if s >= len(sequence):
                 break
-            l += 1
-        if sum == k:
-            answers.append([l, r])
-    
-    answers.sort(key=lambda x: (x[1]-x[0], x[0]))
-    return answers[0]
+        
+    answer.sort(key = lambda x : (x[1]-x[0],x[0]))
+    return answer[0]
