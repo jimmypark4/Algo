0306b890a99ff63400580a17d9866918fa78e605
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-06-24 21:46:41 +0900

    [Silver II] Title: 랜선 자르기, Time: 452 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -16,9 +16,9 @@ def binary_search(arr,N):
         if a < N: #타겟보다 a가 작은 경우, 더 작게 length 감소
             en = mid -1
         elif a >= N:#length 늘려보기
-            st = mid
+            st = mid #여기까지가 N최대 이므로 그냥 st= mid로 진행!
 
-    return st
+    return st #끝났으면은 st을 반환 mid하니까 오류뜸. #st가 마지막N 이 만족한 곳이기 떄문인거같음
 print(
 binary_search(arr,N)
 )
