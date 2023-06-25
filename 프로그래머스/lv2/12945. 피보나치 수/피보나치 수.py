f3d9c636522a52846ae99bc9ac1c278fbd438d16
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-06-25 18:21:54 +0900

    [level 2] Title: 피보나치 수, Time: 478.67 ms, Memory: 439 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,11 @@
+def solution(n):
+    dp = [-1] * (n+1)
+    dp[0] , dp[1] = 0,1
+    
+    for i in range(2,n+1):
+        if dp[i] == -1:
+            dp[i] = dp[i-1] + dp[i-2]
+    
+    
+    answer = dp[n] % 1234567
+    return answer
