707614745237d2abb6eae7bedcee5c93282f0303
jimmy_park <84165850+jimmypark4@users.noreply.github.com>
2023-06-30 17:29:29 +0900

    [Gold V] Title: 토마토, Time: 2544 ms, Memory: 98576 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,9 +1,15 @@
+"""
+토마토 : BFS 문제
+1. 배열을 돌면서 토마토가 없는 nomato을 세고, 토마토가 있으면 큐에 해당 위치를 넣음
+2. 큐에 있는 위치들을 하나씩 popleft()하면서 상하좌우로 움직이며 노마토가 있는 지 체크
+3. 노마토 이면 nomato 카운트를 하나 지우고, 해당 칸을 토마토로 채우고 큐에 해당 위치를 넣음
+4. 만약 노마토가 0이 되거나, 큐가 비어있게 되면 반복을 종료 후, 결과 출력!
+"""
 from collections import deque
 dx = [+1, 0, 0,-1]
 dy = [ 0,+1,-1, 0]
 m , n  = map(int,input().split())
 arr = list( list(map(int,input().split())) for _ in range(n))
-# print(*arr)
 
 q =deque()
 wall = 0
@@ -19,7 +25,6 @@ for i in range(n):
         else:
             tomato += 1
             q.append((i,j))
-# print(q,nomato,tomato)
 if nomato == 0: print(0);exit()
 
 while q:
@@ -35,6 +40,6 @@ while q:
             answer = arr[nx][ny] -1
             break
         q.append((nx,ny))
-    # print(*arr,q,sep='\n')
+    
 
 print(-1) if nomato != 0 else print(answer)
